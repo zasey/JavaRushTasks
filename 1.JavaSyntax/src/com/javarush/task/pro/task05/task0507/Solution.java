d54f6f74fdfe5215fc4fed44e3eaa05106b3a6dd
zaseyka <zasey96@gmail.com>
2021-03-23 23:20:53 +0300

    add all files

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task05.task0507;
+
+import java.util.Scanner;
+
+/* 
+Максимальное из N чисел
+*/
+
+public class Solution {
+    public static int[] array;
+
+    public static void main(String[] args) throws Exception {
+        Scanner console = new Scanner(System.in);
+        int N = console.nextInt();
+        array = new int[N];
+        for(int i = 0; i < N ; i++)
+            array[i] = console.nextInt();
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < N ; i++)
+            if(array[i] > max)
+                max = array[i];
+        System.out.println(max);
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.pro.task05.task0508;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/* 
+Удаляем одинаковые строки
+*/
+
+public class Solution {
+    public static String[] strings;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        strings = new String[10];
+        for (int i = 0; i < 10; i++) {
+            strings[i] = scanner.nextLine();
+        }
+
+        for (int i = 0; i < strings.length; i++) {
+            String currentString = strings[i];
+            for (int j = i + 1; j < strings.length; j++) {
+                if (currentString == null) {
+                    break;
+                }
+                if (currentString.equals(strings[j])) {
+                    strings[j] = null;
+                    strings[i] = null;
+                }
+            }
+        }
+        Arrays.stream(strings).forEach(System.out::println);
+    }
+}
