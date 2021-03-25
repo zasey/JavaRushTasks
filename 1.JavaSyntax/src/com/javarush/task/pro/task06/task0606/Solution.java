967f933e598eaa69b35f396e955bde1b6bd3bde2
zaseyka <zasey96@gmail.com>
2021-03-25 19:33:06 +0300

    add all files

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+    public static void universalMethod(int a){
+
+    }
+    public static void universalMethod(int a, int b){
+
+    }
+    public static void universalMethod(int a, int b, int c){
+
+    }
+    public static void universalMethod(int[] a, int b){
+
+    }
+    public static void universalMethod(int[] a, int b, int c){
+
+    }
+    public static void universalMethod(double a){
+
+    }
+    public static void universalMethod(double a, double[] b){
+
+    }
+    public static void universalMethod(double[] a, double b){
+
+    }
+    public static void universalMethod(boolean a){
+
+    }
+    public static void universalMethod(String s){
+
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int a;
+    short b;
+    byte c;
+    long d;
+    float e;
+    double f;
+    boolean g;
+    char h;
+}
