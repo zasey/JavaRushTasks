c87fc8ebcecd6c74b31c3dc951c6221cf2665899
zaseyka <zasey96@gmail.com>
2021-03-25 20:38:50 +0300

    add all files

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0801/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task08.task0801;
+
+import static java.lang.Math.toRadians;
+
+/* 
+Утильный класс: часть 1
+*/
+
+public class Solution {
+
+    public static double sin(double a) {
+        //напишите тут ваш код
+        return Math.sin(toRadians(a));
+    }
+
+    public static double cos(double a) {
+        //напишите тут ваш код
+        return Math.cos(toRadians(a));
+    }
+
+    public static double tan(double a) {
+        //напишите тут ваш код
+        return Math.tan(toRadians(a));
+    }
+}
