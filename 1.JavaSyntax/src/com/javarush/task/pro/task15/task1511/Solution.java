35700cd6be884feaa0516d25dc7a81c25cf68135
zaseyka <zasey96@gmail.com>
2021-04-04 13:32:54 +0300

    add all files

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1511/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.pro.task15.task1511;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+/* 
+Пишем символы в файл
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        char[] chars = args[0].toCharArray();
+        try (InputStream stream = System.in;
+             Scanner scanner = new Scanner(stream);
+             BufferedWriter buff = Files.newBufferedWriter(Path.of(scanner.nextLine()))) {
+            buff.write(chars);
+        } catch (IOException e) {
+            System.out.println("Something went wrong : " + e);
+        }
+    }
+}
+
+
