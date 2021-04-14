fb7df722db2da5768ccabd3c80a7125ea80c44b5
zaseyka <zasey96@gmail.com>
2021-04-14 19:12:39 +0300

    add all files

--- 2.JavaCore/src/com/javarush/task/task13/task1326/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1326/Solution.java
@@ -12,7 +12,25 @@ import java.util.Scanner;
 */
 
 public class Solution {
-    public static void main(String[] args) {
-        // напишите тут ваш код
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sourceFileName = reader.readLine();
+        java.io.FileInputStream fileInputStream = new java.io.FileInputStream(sourceFileName);
+
+        Scanner scanner = new Scanner(fileInputStream);
+        List<Integer> data = new ArrayList<Integer>();
+
+        while (scanner.hasNext()) {
+            int value = scanner.nextInt();
+            if (value % 2 == 0) data.add(value);
+        }
+
+        Collections.sort(data);
+
+        for (Integer value : data)
+            System.out.println(value);
+
+        scanner.close();
+        fileInputStream.close();
     }
 }
